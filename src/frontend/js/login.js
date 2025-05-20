80614afb14b4587ceeb8c7fefdf0c5b5e0106436
mirmoa <moamir_ins@hotmail.com>
2025-05-20 18:33:15 +0900

    Update login.js

--- src/frontend/js/login.js
+++ src/frontend/js/login.js
@@ -10,7 +10,7 @@
     .login-overlay {
       position: fixed;
       top: 0; left: 0; right: 0; bottom: 0;
-      background: rgba(0,0,0,0.6);
+      background: rgba(0,0,0);
       display: flex;
       justify-content: center;
       align-items: center;
